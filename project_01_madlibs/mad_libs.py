de6194e9d1668ccd4bcf6b23edc199c51ddbfdb7
Nihal khan Ghauri <144270394+NihalGhauri@users.noreply.github.com>
2025-04-14 01:39:56 +0500

    Update mad_libs.py

--- project_01_madlibs/mad_libs.py
+++ project_01_madlibs/mad_libs.py
@@ -1,7 +1,7 @@
 adj = input("Adjective: ")
 verb1 = input("Verb: ")
 verb2 = input("Verb: ")
-famous_person = input("famous person: ");
+famous_person = input("famous person:  ");
 
 madlib = f"Computer Programming is so {adj}! It makes me so exicited all the time beacause \
 I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}."
